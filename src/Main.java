41f05cf40b8dc4df9f3e48240bbf0fa8c58ebd06
zurira <20243ds036@utez.edu.mx>
2025-05-27 23:11:22 -0600

    Realice el llenado de datos de las demas instancias

--- src/Main.java
+++ src/Main.java
@@ -21,10 +21,48 @@ public class Main {
         estudiante2.setNota(3, 70);
         estudiante2.setNota(4, 80);
 
+        estudiante3.setNombre("Viviana");
+        estudiante3.setMatricula("e003");
+        estudiante3.setNota(0, 90);
+        estudiante3.setNota(1, 80);
+        estudiante3.setNota(2, 80);
+        estudiante3.setNota(3, 70);
+        estudiante3.setNota(4, 80);
 
+        estudiante4.setNombre("Tania");
+        estudiante4.setMatricula("e004");
+        estudiante4.setNota(0, 70);
+        estudiante4.setNota(1, 100);
+        estudiante4.setNota(2, 80);
+        estudiante4.setNota(3, 70);
+        estudiante4.setNota(4, 80);
+
+        estudiante5.setNombre("Lizzete");
+        estudiante5.setMatricula("e005");
+        estudiante5.setNota(0, 90);
+        estudiante5.setNota(1, 70);
+        estudiante5.setNota(2, 80);
+        estudiante5.setNota(3, 70);
+        estudiante5.setNota(4, 80);
 
         System.out.println("Nombre: " + estudiante1.getNombre());
         System.out.printf("Promedio: %.2f\n", estudiante1.calcularPromedio());
         System.out.println("¿Aprobado?: " + (estudiante1.aprobado() ? "Sí" :"No"));
+
+        System.out.println("Nombre: " + estudiante2.getNombre());
+        System.out.printf("Promedio: %.2f\n", estudiante2.calcularPromedio());
+        System.out.println("¿Aprobado?: " + (estudiante2.aprobado() ? "Sí" :"No"));
+
+        System.out.println("Nombre: " + estudiante3.getNombre());
+        System.out.printf("Promedio: %.2f\n", estudiante3.calcularPromedio());
+        System.out.println("¿Aprobado?: " + (estudiante3.aprobado() ? "Sí" :"No"));
+
+        System.out.println("Nombre: " + estudiante4.getNombre());
+        System.out.printf("Promedio: %.2f\n", estudiante4.calcularPromedio());
+        System.out.println("¿Aprobado?: " + (estudiante4.aprobado() ? "Sí" :"No"));
+
+        System.out.println("Nombre: " + estudiante5.getNombre());
+        System.out.printf("Promedio: %.2f\n", estudiante5.calcularPromedio());
+        System.out.println("¿Aprobado?: " + (estudiante5.aprobado() ? "Sí" :"No"));
     }
 }
